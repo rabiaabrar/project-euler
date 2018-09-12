62ffd22c97c9668993e1883b28c7430b4348167d
rabiaabrar <rabia.abrar@gmail.com>
2018-09-12 15:30:22 +1000

    It does something meaningful now but by brute force
    It just plays the game a number of times and takes avg of number of turns from each game. This problem would be correctly solved by use of statistics instead.

--- Problem227.cs
+++ Problem227.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 
 // This is a basic solution to Project Euler problem called "The Chase"
+// It isn't really a full solution because I've just written code to play the game a number of times and take avg of number of turns from each game
+// This would be correctly solved by use of statistics
 // https://projecteuler.net/problem=227
 
 // To execute C#, please define "static void Main" on a class
@@ -14,22 +16,36 @@ class Solution
     
     static void Main(string[] args)
     {        
-        int players = 4;
-        int p1=1, p2=players/2+1;
-        int turns =0;  
+        int players = 100;
+        int trials = 10000;
+        int totalTurns = 0;
+        //int[] results = new int[trials];
         
-        while(p1!=p2)
-        {
-            int d1 = RollDie();
-            int d2 = RollDie();
-            p1 = (p1+PassDie(d1))%players;
-            p2 = (p2+PassDie(d2))%players;            
+        for (int i=0; i<trials; i++)
+        {            
+            int p1 = 0, p2 = players/2;
+            int turns = 0;
             
-            Console.WriteLine("Dice: {0}, {1}. Players: {2}, {3}", d1, d2, p1, p2);
-            
-            turns++;            
+            while(p1!=p2)
+            {
+                //Roll two dice, one for each current player
+                int d1 = RollDie();
+                int d2 = RollDie();
+                
+                //Pass the dice to either left, right or keep it
+                //The output from PassDie is ether -1, 0 or 1
+                //We add "players" before taking modulus to account for a potential -1 value
+                p1 = (p1+PassDie(d1)+players) % players;
+                p2 = (p2+PassDie(d2)+players) % players;
+                turns++;
+                
+                //Console.WriteLine("Dice: {0}, {1}. Players: {2}, {3}", d1, d2, p1, p2);
+            }            
+            //Record number of turns in an array to take average of in the end
+            //results[i]=turns;
+            totalTurns+=turns;
         }
-        Console.WriteLine("Turns: {0}", turns);
+        Console.WriteLine("Average turns: {0}", (double)totalTurns/trials);
         Console.WriteLine("That's all folks.");
     }
     
@@ -47,4 +63,3 @@ class Solution
         else return 0;
     }
 }
-
